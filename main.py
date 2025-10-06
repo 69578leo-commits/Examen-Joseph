10dd238b532ce8199aaa6f342ce42563419b0d92
Joseph Mares Muñoz <69578leo@universidaddeleon.edu.mx>
2025-10-06 08:04:47 -0600

    Agregue el area del circulo e hice pruebas

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from clases.areas import area_rectangulo,area_triangulo
+from clases.areas import area_rectangulo, area_triangulo, area_circulo
 
 def main():
     while True:
         print("\n--- CÁLCULO DE ÁREAS ---")
         print("1. Área de un triángulo")
         print("2. Área de un rectángulo")
-        print("3. Salir")
+        print("3. Área de un círculo")
+        print("4. Salir")
 
-        opcion = input("Elige una opción (1-3): ")
+        opcion = input("Elige una opción (1-4): ")
 
         if opcion == "1":
             base = float(input("Ingresa la base del triángulo: "))
@@ -22,6 +23,11 @@ def main():
             print(f"El área del rectángulo es: {area:.2f}")
 
         elif opcion == "3":
+            radio = float(input("Ingresa el radio del círculo: "))
+            area = area_circulo(radio)
+            print(f"El área del círculo es: {area:.2f}")
+
+        elif opcion == "4":
             print("Saliendo del programa...")
             break
 
